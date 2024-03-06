e48c51b71d8cbf36811d6b479dff444cb84247eb
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-03-06 17:42:57 -0300

    test livro description

--- tests/descricao_livro/test_descricao_livro.py
+++ tests/descricao_livro/test_descricao_livro.py
@@ -1,4 +1,9 @@
 from src.livro.livro import Livro
 
+
 def test_descricao_livro():
-    
+    livro = Livro("O Senhor dos Anéis", "J.R.R. Tolkien", 1000)
+    assert (
+        repr(livro)
+        == "O livro O Senhor dos Anéis de J.R.R. Tolkien possui 1000 páginas."
+    )
--- tests/livro/test_livro.py
+++ tests/livro/test_livro.py
@@ -6,7 +6,3 @@ def test_cria_livro(livro):
     assert livro.titulo == "O Senhor dos Anéis"
     assert livro.autor == "J.R.R. Tolkien"
     assert livro.paginas == 1000
-    assert (
-        repr(livro)
-        == "O livro O Senhor dos Anéis de J.R.R. Tolkien possui 1000 páginas."
-    )
